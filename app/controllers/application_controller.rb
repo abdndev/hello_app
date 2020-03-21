b732765ea507736683d6be9dbac67ca82f4837c4
ABDNdev <igor@abidin.ru>
2020-03-21 21:46:42 +0000

    Insert new method to application controller, modify routes.rb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     
     def hello
-      render html: "hello, my world!"
+      render html: "hello, my world! Привет, мир!"
+    end
+    
+    def goodbye
+      render html: "Goodbye, мир!"
     end
 end
